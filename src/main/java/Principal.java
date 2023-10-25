6a6ad86d294e41d7327ec7f43c75208a535805fc
JOBFER1 <jobfer@outlook.es>
2023-10-25 11:34:43 +0200

    Sube clase Secundaria

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -4,5 +4,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Principal!!");
 		System.out.println("Hola rama master!!");
+		
+		System.out.println("Código nuevo");
 	}
 }
---
+++ src/main/java/Secundaria.java
@@ -0,0 +1,11 @@
+
+public class Secundaria {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Secundaria!!");
+		System.out.println("Hola rama master!!");
+
+	}
+
+}
